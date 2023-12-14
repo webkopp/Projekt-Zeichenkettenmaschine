34481c518bbec1df8e620ee5d9adc61f49c513d8
webkopp <nakopps@googlemail.com>
2023-12-14 16:41:46 +0100

    aZeichenUpdate

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,32 @@
 // Über Radio-Buttons kann spezifiziert werden, ob die Trennung vor oder nach der Trennungs-Zeichenkette geschehen soll.
 // Klingt kompliziert? Schau es dir in der Vorschau an!
 
-let nancy = () => {
-    let text1 = document.querySelector(#chain).value
-    let text2 = document.querySelector(#letter).value
-    let check1 = document.querySelector(#before).checked
-    let check2 = document.querySelector(#behind).checked
-    let out1 = document.querySelector(#outi1)
-    let out2 = document.querySelector(#outi2)
+const nancy = () => {
+    let text1 = document.querySelector("#chain").value
+    let text2 = document.querySelector("#letter").value
+    let check1 = document.querySelector("#before").checked
+    let check2 = document.querySelector("#behind").checked
+    let out1 = document.querySelector("#outi1")
+    let out2 = document.querySelector("#outi2")
 
     let atIndex = text1.indexOf(text2)
+
+    let chain1 = text1.slice(0, atIndex)
+    let chain2 = text1.slice(atIndex)
+    let chain3 = text1.slice(0, atIndex +1)
+    let chain4 = text1.slice(atIndex +1)
+
+    switch(true) {
+        case check1:
+            out1.innerHTML = chain1
+            out2.innerHTML = chain2
+            break;
+        case check2:
+            out1.innerHTML = chain3
+            out2.innerHTML = chain4
+            break;
+        default: out1.innerHTML = "test";
+    }
 }
+
+nancy()
